cd39eb9be35a1d50b387273e19dfcaa816e23f6c
Andrew Rees <andrewtrevor.rees@gmail.com>
2018-08-31 18:33:13 +0100

    js functions now printing to html dom

--- Scripts/haikus.js
+++ Scripts/haikus.js
@@ -4,15 +4,18 @@
 
 
 
-//Reset Button on submit own haiku can only be pressed twice
+
 
 document.addEventListener("DOMContentLoaded", () => {
+
+//Reset Button on submit own haiku can only be pressed twice 
+
     var resetVis = "visible";
     var buttonCount = 0;
 
     function showHideReset() {
         document.getElementById("reset").style.visibility = resetVis;
-        console.log("Reset Button is " + resetVis);
+        //console.log("Reset Button is " + resetVis);
     }
     showHideReset()
 
@@ -32,117 +35,346 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
+    //--------------------------------------------------------------------------------//
+
+
+    //make Submit button check there is data in each line
+    //Turn user-gen haiku into text on page
+    
+    var userHaikuVis = "hidden";//hides the element before button pressed
+
+    function showHideUserHaiku() {
+        document.getElementById("Display-4").style.visibility = userHaikuVis; //hides the element so the CSS doesn't appear on page
+        document.getElementById("Keep Haiku 4").style.visibility = userHaikuVis;
+    }
+    showHideUserHaiku()
+
+    function checkSubmission() {
+        var lineOne = document.getElementById("line one").value
+        console.log(lineOne)
+        var lineTwo = document.getElementById("line two").value
+        console.log(lineTwo)
+        var lineThree = document.getElementById("line three").value
+        console.log(lineThree)
 
 
 
-//make Submit button check there is data in each line
-//Turn user-gen haiku into text on page
-//STILL TO DO
-//// Allow 'reset' button to also remove the text from the page - but can't work out how to give the element I'm creating an ID when I create it
+        if (lineOne.length > 0 && lineTwo.length > 0 && lineThree.length > 0) {
+            userHaikuVis = "visible"; //shows the element
+            showHideUserHaiku();
 
+            var pOne = document.createElement("p");
+            var pTwo = document.createElement("p");
+            var pThree = document.createElement("p");
+            var element = document.getElementById("Display-4");
+            var useLineOne = document.createTextNode(lineOne);
+            var useLineTwo = document.createTextNode(lineTwo);
+            var useLineThree = document.createTextNode(lineThree);
 
+            pOne.appendChild(useLineOne);
+            pTwo.appendChild(useLineTwo);
+            pThree.appendChild(useLineThree);
+            element.appendChild(pOne);
+            element.appendChild(pTwo);
+            element.appendChild(pThree);
 
+            //Put text on page:
+            ////turn each box into a string if lineOne Two Three aren't already
+            ////display each string in html on the page
+            ////format those strings in html and scss
+        } else {
+            alert("You need to add something into each box - allow that power to glisten, child...")
+        }
 
-
-
-function checkSubmission() {
-    var lineOne = document.getElementById("line one").value
-    console.log(lineOne)
-    var lineTwo = document.getElementById("line two").value
-    console.log(lineTwo)
-    var lineThree = document.getElementById("line three").value
-    console.log(lineThree)
-
-    if (lineOne.length > 0 && lineTwo.length > 0 && lineThree.length > 0) {
-
-        var pOne = document.createElement("p");
-        var pTwo = document.createElement("p");
-        var pThree = document.createElement("p");
-        var element = document.getElementById("Haiku Four");
-        var useLineOne = document.createTextNode(lineOne);
-        var useLineTwo = document.createTextNode(lineTwo);
-        var useLineThree = document.createTextNode(lineThree);
-        
-        pOne.appendChild(useLineOne);
-        pTwo.appendChild(useLineTwo);
-        pThree.appendChild(useLineThree);
-        element.appendChild(pOne);
-        element.appendChild(pTwo);
-        element.appendChild(pThree);
-
-        //Put text on page:
-        ////turn each box into a string if lineOne Two Three aren't already
-        ////display each string in html on the page
-        ////format those strings in html and scss
-    } else {
-        alert("You need to add something into each box - allow that power to glisten, child...")
     }
 
-}
-
-var submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', function (event) {
-    checkSubmission();
-});
+    var submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', function (event) {
+        checkSubmission();
+    });
 
 
+    //-------------------------------------------------------------//
 
 
-
-//Random Haiku Generation
-////create objects with words - fine
-//// randomly generate them - trouble with this, copied formula isn't random between 0 and 7
-//// print each word - fine
-//// put each word into the array of the line eg firstLine[]
-//// rules for words:
-//////start random
-//////after adjective comes noun
-//////after adverb comes verb
-//////after join comes adjective
-//////after verb comes join
-//////after noun comes adverb
+    //Random Haiku Generation////
 
 
-//// implement these rules
-//// line 1 and 3 are 5 words, line 2 is 7 words
-////further - give each word a property (syllables) and use this to calculate line length, not word length;
+    //STILL TO DO
+    ////line.trim() doesn't work so each line has a space at end
+    ////further - give each word a property (syllables) and use this to calculate line length, not word length;
+    ////further - capitalise first letter of line (var firstLineUC doesn't work)
+    ////clear the html element when pressing the button, then re-displaying it with new value (if (randomHaikuVis === "visible") doesn't work)
+
+    // VARIBLES AND ARRAYS
+    let firstLine = ""
+    let secondLine = ""
+    let thirdLine = ""
+
+
+    const nouns = ["world", "money", "monkey", "honey", "south", "breath", "wind", "love", "I", "you", "they", "tower", "cloud", "sun", "beyond", "trees", "melody", "hands"];
+    const joins = ["on", "with", "in", "for", "will", "together", "apart", "far", "at"];
+    const verbs = ["fly", "feel", "think", "love", "go", "run", "slide", "jostle", "shout", "sing", "sit", "breathe", "swoop"];
+    const adjectives = ["wild", "constant", "jubilant", "red", "pale", "charmed", "blue", "green", "strong", "hopeful"];
+    const adverbs = ["slowly", "gently", "fleetingly", "movingly", "calmly", "suddenly", "wholly", "lithely", "timely"];
+    var randomNumber;
+    var randomHaikuVis = "hidden"; //hides the element before button pressed
+    var numberOfRandomHaikus = 0 //stores number of haikus generated        
+    var which = "" //holds which type of word has just been used
+
+    function showHideRandomHaiku() {
+        document.getElementById("Display-5").style.visibility = randomHaikuVis; //hides the element so the CSS doesn't appear on page
+        document.getElementById("Keep Haiku 5").style.visibility = randomHaikuVis; 
+    } 
+    showHideRandomHaiku()
+
+    
+
+    var randomHaiku = document.getElementById('Random Haiku');
+    randomHaiku.addEventListener('click', function (event) {
+
+    if (randomHaikuVis === "visible") { //doesn't work?
+        randomHaikuVis = "hidden"
+        showHideRandomHaiku()
+    }
+
+    function generateHaiku () { //generates the haiku
+        randomHaikuVis = "visible"; //shows the element
+        showHideRandomHaiku();
+        numberOfRandomHaikus ++
+        console.log(numberOfRandomHaikus);
+
+        ((whichLine) => { //generates Line One
+            console.log(`Line ${whichLine}:`)
+            haikuStart(whichLine); //starts the poem
+            nextWord(whichLine); //displays a word depending on the type of the previous word
+            nextWord(whichLine); //ditto
+            nextWord(whichLine);
+            nextWord(whichLine);
+            firstLine.trim(); //doesn't work??
+            firstLine += ","
+            var firstLineUC = firstLine.charAt(0).toUpperCase() + firstLine.substr(1); //also doesn't work?
+            console.log(firstLineUC); //logs the array of words generated by the functions above
+            displayLine(whichLine); //displays the line in the right place in the HTML DOM
+            firstLine = ""; //clears the array so if you press Generate button again, it shows a new haiku
+        })(1) //calls the function and sets whichLine to 1
+
+        function generateTwo(whichLine) {
+            console.log(`Line ${whichLine}:`)
+            nextWord(whichLine);
+            nextWord(whichLine);
+            nextWord(whichLine);
+            nextWord(whichLine);
+            nextWord(whichLine);
+            nextWord(whichLine);
+            nextWord(whichLine);
+            secondLine.trim();
+            secondLine += ","
+            console.log(secondLine)
+            displayLine(whichLine)
+            secondLine = "";
+        }
+        generateTwo(2);
+
+        function generateThree(whichLine) {
+            console.log(`Line ${whichLine}:`)
+            nextWord(whichLine);
+            nextWord(whichLine);
+            nextWord(whichLine);
+            nextWord(whichLine);
+            nextWord(whichLine);
+            thirdLine.trim();
+            thirdLine += "."
+            console.log(thirdLine)
+            displayLine(whichLine)
+            thirdLine = "";
+        };
+        generateThree(3);
+
+     }  
+       generateHaiku()
+
+    // function runGenerateHaiku () {
+    //         if (numberOfRandomHaikus = 0) {
+    //             generateHaiku();
+    //         } else if (numberOfRandomHaikus = 1) {
+    //             var parent = document.getElementById("Haiku Five");
+    //             var child = document.getElementById("Display-5");
+    //             parent.removeChild(child);
+    //             generateHaiku();      
+    //         }
+    //     }
+    //     runGenerateHaiku()
+
+    });
+
+    
+
+    //HELPER FUNCTIONS - GENERATING A RANDOM NUMBER AND PASSING THE WORD TO THE APPROPRIATE PLACE IN THE HTML DOM//
+
+    function generateRandom(min, max) { //generates a random number for use elsewhere
+        randomNumber = (Math.floor(Math.random() * (max - min)) + min);
+    };
+
+    function displayLine(whichLine) { //displays the text in the right place
+        let para = document.createElement("p");
+        para.setAttribute('id','Random-Haiku-Line');
+        let element = document.getElementById("Display-5");
+        
+        if (whichLine == 1) {
+            let node = document.createTextNode(firstLine);
+            para.appendChild(node);
+            element.appendChild(para);
+        } else if (whichLine == 2) {
+            let node = document.createTextNode(secondLine);
+            para.appendChild(node);
+            element.appendChild(para);
+        } else if (whichLine == 3) {
+            let node = document.createTextNode(thirdLine);
+            para.appendChild(node);
+            element.appendChild(para);
+        } else {
+            console.log("whichLine variable has no value")
+        }
+        
+    };
+
+    function haikuStart(whichLine) { //starts the haiku with a random word
+
+        x = Math.floor(Math.random() * 5);
+        if (x == 0) {
+            generateNoun(whichLine)
+            which = "noun"
+        } else if (x == 1) {
+            generateJoin(whichLine)
+            which = "join"
+        } else if (x == 2) {
+            generateVerb(whichLine)
+            which = "verb"
+        } else if (x == 3) {
+            generateAdjective(whichLine)
+            which = "adjective"
+        } else if (x == 4) {
+            generateAdverb(whichLine)
+            which = "adverb"
+        }
+    };
+
+    function nextWord (whichLine) { //chooses the next word depending on the last
+
+        switch(which) {
+            case "noun":
+                generateAdverb(whichLine)
+                which = "adverb"
+                break;
+            case "join":
+                generateAdjective(whichLine)
+                which = "adjective"
+                break;
+            case "verb":
+                generateJoin(whichLine)
+                which = "join"
+                break;
+            case "adjective":
+                generateNoun(whichLine)
+                which = "noun"
+                break;
+            case "adverb":
+                generateVerb(whichLine)
+                which = "verb"
+                break;
+        }
+
+    };
+
+    //GENERATING THE WORDS//
+
+    function generateNoun(whichLine) {
+        generateRandom(0, nouns.length)
+        let thisNoun = `${nouns[randomNumber]} `
+        //console.log(`thisNoun: ${thisNoun}`)
+        if (whichLine == 1) {
+            //firstLine.push(thisNoun)
+            firstLine +=thisNoun
+            //console.log(firstLine)
+        } else if (whichLine == 2) {
+            secondLine += thisNoun
+        } else if (whichLine == 3) {
+            thirdLine +=thisNoun;
+        } else {
+            console.log("whichLine has no value")
+        }
+
+    };
+
+    function generateJoin(whichLine) {
+        generateRandom(0, joins.length)
+        let thisJoin = `${joins[randomNumber]} `
+        //console.log(`thisJoin: ${thisJoin}`)
+        if (whichLine == 1) {
+            //firstLine.push(thisJoin)
+            firstLine +=thisJoin
+            //console.log(firstLine)
+        } else if (whichLine == 2) {
+            secondLine +=thisJoin
+        } else if (whichLine == 3) {
+            thirdLine +=thisJoin;
+        } else {
+            console.log("whichLine has no value")
+        }
+    };
+
+    function generateVerb(whichLine) {
+        generateRandom(0, verbs.length)
+        let thisVerb = `${verbs[randomNumber]} `
+        //console.log(`thisVerb: ${thisVerb}`)
+        if (whichLine == 1) {
+            //firstLine.push(thisVerb)
+            firstLine +=thisVerb
+            //console.log(firstLine)
+        } else if (whichLine == 2) {
+            secondLine += thisVerb
+        } else if (whichLine == 3) {
+            thirdLine += thisVerb;
+        } else {
+            console.log("whichLine has no value")
+        }
+    };
+
+    function generateAdjective(whichLine) {
+        generateRandom(0, adjectives.length)
+        let thisAdjective = `${adjectives[randomNumber]} `
+        //console.log(`thisAdjective: ${thisAdjective}`)
+        if (whichLine == 1) {
+            //firstLine.push(thisAdjective)
+            firstLine += thisAdjective
+            //console.log(firstLine)
+        } else if (whichLine == 2) {
+            secondLine +=thisAdjective
+        } else if (whichLine == 3) {
+            thirdLine += thisAdjective;
+        } else {
+            console.log("whichLine has no value")
+        }
+    };
+
+    function generateAdverb(whichLine) {
+        generateRandom(0, adverbs.length)
+        let thisAdverb = `${adverbs[randomNumber]} `
+        //console.log(`thisAdverb: ${thisAdverb}`)
+        if (whichLine == 1) {
+            //firstLine.push(thisAdverb)
+            firstLine +=thisAdverb
+            //console.log(firstLine)
+        } else if (whichLine == 2) {
+            secondLine += thisAdverb
+        } else if (whichLine == 3) {
+            thirdLine += thisAdverb;
+        } else {
+            console.log("whichLine has no value")
+        }
+    };
 
 
 }, false);
 
-
-const firstLine = [];
-const secondLine = [];
-const thirdLine = [];
-
-const nouns = ["world", "money", "monkey", "honey", "south", "wind", "love", "I", "you", "they", "tower"];
-const joins = ["on", "with", "in", "for", "will"];
-const verbs = ["fly", "feel", "think", "love", "go"];
-const adjectives = ["wild", "constant", "hoping", "jubilant"];
-const adverbs = ["slowly", "gently", "fleetingly", "movingly", "calmly"];
-const words = [nouns, joins, verbs, adjectives, adverbs]
-var randomNumber;
-var randomNumberResult = [];
-
-function generateOne (min, max) {
-    for (let i = 0; i < 5; i++) {
-        randomNumber = (Math.floor(Math.random() * (max - min + 1) ) + min);
-        console.log(randomNumber);
-        //randomNumberResult.push(randomNumber);
-        
-    }
-
-}
-
-//firstLine Generation and pushing
-//running the function to produce the random numbers.
-generateOne(0, nouns.length);
-generateOne(0, joins.length);
-generateOne(0, verbs.length);
-generateOne(0, adjectives.length);
-generateOne(0, adverbs.length);
-
-//displaying the words that correspond to the above numbers
-console.log(`${adjectives[randomNumber]} ${nouns[randomNumber]} ${joins[randomNumber]} ${adverbs[randomNumber]} ${verbs[randomNumber]}`);
-//console.log(randomNumberResult);
-//console.log(Math.round(Math.random()));
