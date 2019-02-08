dc0d11ac5530d09673d594b0fb079c325efe681b
Andrew Rees <andrewtrevor.rees@gmail.com>
2019-02-08 17:18:25 +0000

    partial refactor and jQuery

--- scriptstemp/litcrit.js
+++ scriptstemp/litcrit.js
@@ -4,6 +4,15 @@
 //// think I can get rid of the triplicate creation of the poets using a for loop, so using the 'i' eg poet[i]
 /// can i use a poet class (see practice.js)?
 
+class Poet {
+    constructor(index, name, webPage, snippet) {
+        this.index = index;
+        this.name = name;
+        this.webPage = webPage;
+        this.snippet = snippet;
+    };
+};
+
 const authors = [
     "Adam Lindsay Gordon",
     "Alan Seeger",
@@ -136,157 +145,71 @@ const authors = [
     "William Wordsworth"
 ]
 
+function threeRandomNumbers(min, max) {
+    randomNumberOne = (Math.floor(Math.random() * (max - min)) + min);
+    randomNumberTwo = (Math.floor(Math.random() * (max - min)) + min);
+    randomNumberThree = (Math.floor(Math.random() * (max - min)) + min);
+};
+do {
+    threeRandomNumbers(0, authors.length)
+} while ((randomNumberOne === randomNumberTwo) ||
+    (randomNumberOne === randomNumberThree) ||
+    (randomNumberThree === randomNumberTwo));
+
+var firstPoet = authors[randomNumberOne];
+var secondPoet = authors[randomNumberTwo];
+var thirdPoet = authors[randomNumberThree];
+
+
+var poetsInformation = [];
+
+function displayHeader(poetName, index) {
+    var header = $("<h3>").text(poetName);
+    $(`#getPoets${index}`).append(header);
+};
+
+function displayLink(hrefUrl, index) {
+    var link = $("<a>").text("Link to Poet's wikipedia").attr('href', hrefUrl).attr("target", "_blank")
+    $(`#getPoets${index}`).append(link);
+};
+
+function displaySnippet(websiteSnippet, index) {
+    var snip = $("<p>").text(websiteSnippet);
+    $(`#getPoets${index}`).append(snip);
+};
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    var showPoets = document.getElementById('poet button');
-    showPoets.addEventListener('click', function (event) {
+    $("#poetButton").click(function () {
 
-        //need to require the poets array
+        poetsInformation.push(new Poet(1, firstPoet));
+        poetsInformation.push(new Poet(2, secondPoet));
+        poetsInformation.push(new Poet(3, thirdPoet));
 
-        //random number IIFE  
-        ((min, max) => {
-            randomNumberOne = (Math.floor(Math.random() * (max - min)) + min);
-            randomNumberTwo = (Math.floor(Math.random() * (max - min)) + min);
-            randomNumberThree = (Math.floor(Math.random() * (max - min)) + min);
-        })(0, authors.length)
+        poetsInformation.forEach(value => {
+            var searchName = value.name.replace(" ", "%20").replace(" ", "%20")
+            var searchUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchName}&utf8=&format=json`;
+            var webPageName = value.name.replace(" ", "_").replace(" ", "_")
+            value.webPage = `https://en.wikipedia.org/wiki/${webPageName}`;
+            fetch(searchUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (parsedResponseWiki) {
+                    value.snippet = `${parsedResponseWiki.query.search[0].snippet}... click on the link to find out more`;
+                    displayHeader(value.name, value.index);
+                    displayLink(value.webPage, value.index);
+                    displaySnippet(value.snippet, value.index);
+                });
+        });
 
-        let firstPoet = authors[randomNumberOne];
-        let firstPoetSplit = firstPoet.split(" ");
-        console.log(firstPoet)
 
-        if (firstPoetSplit.length === 1) {
-            var poet1 = `${firstPoetSplit[randomNumberOne]}`
-            var url1 = `https://en.wikipedia.org/wiki/${poet1}`
-            var searchUrl1 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${poet1}&utf8=&format=json`
-        } else if (firstPoetSplit.length === 2) {
-            var poet1 = `${firstPoetSplit[0]} ${firstPoetSplit[1]}`
-            var url1 = `https://en.wikipedia.org/wiki/${firstPoetSplit[0]}_${firstPoetSplit[1]}`
-            var searchUrl1 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${firstPoetSplit[0]}%20${firstPoetSplit[1]}&utf8=&format=json`
-        } else if (firstPoetSplit.length === 3) {
-            var poet1 = `${firstPoetSplit[0]} ${firstPoetSplit[1]} ${firstPoetSplit[2]}`
-            var url1 = `https://en.wikipedia.org/wiki/${firstPoetSplit[0]}_${firstPoetSplit[1]}_${firstPoetSplit[2]}`
-            var searchUrl1 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${firstPoetSplit[0]}%20${firstPoetSplit[1]}%20${firstPoetSplit[2]}&utf8=&format=json`
-        } else if (firstPoetSplit.length === 4) {
-            var poet1 = `${firstPoetSplit[0]} ${firstPoetSplit[1]} ${firstPoetSplit[2]} ${firstPoetSplit[3]}`
-            var url1 = `https://en.wikipedia.org/wiki/${firstPoetSplit[0]}_${firstPoetSplit[1]}_${firstPoetSplit[2]}_${firstPoetSplit[3]}`
-            var searchUrl1 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${firstPoetSplit[0]}%20${firstPoetSplit[1]}%20${firstPoetSplit[2]}%20${firstPoetSplit[3]}&utf8=&format=json`
-        } else {
-            var poet1 = "This poet has not got a name, or it's too long"
-            var url1 = "There is no link"
-            var searchUrl3 = "there is no snippet"
-        }
-
-        let secondPoet = authors[randomNumberTwo];
-        let secondPoetSplit = secondPoet.split(" ");
-        console.log(secondPoet)
-        if (secondPoetSplit.length === 1) {
-            var poet2 = `${secondPoetSplit[0]}`
-            var url2 = `https://en.wikipedia.org/wiki/${poet2}`
-            var searchUrl2 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${poet2}&utf8=&format=json`
-        } else if (secondPoetSplit.length === 2) {
-            var poet2 = `${secondPoetSplit[0]} ${secondPoetSplit[1]}`
-            var url2 = `https://en.wikipedia.org/wiki/${secondPoetSplit[0]}_${secondPoetSplit[1]}`
-            var searchUrl2 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${secondPoetSplit[0]}%20${secondPoetSplit[1]}&utf8=&format=json`
-        } else if (secondPoetSplit.length === 3) {
-            var poet2 = `${secondPoetSplit[0]} ${secondPoetSplit[1]} ${secondPoetSplit[2]}`
-            var url2 = `https://en.wikipedia.org/wiki/${secondPoetSplit[0]}_${secondPoetSplit[1]}_${secondPoetSplit[2]}`
-            var searchUrl2 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${secondPoetSplit[0]}%20${secondPoetSplit[1]}%20${secondPoetSplit[2]}&utf8=&format=json`
-        } else if (secondPoetSplit.length === 4) {
-            var poet2 = `${secondPoetSplit[0]} ${secondPoetSplit[1]} ${secondPoetSplit[2]} ${secondPoetSplit[3]}`
-            var url2 = `https://en.wikipedia.org/wiki/${secondPoetSplit[0]}_${secondPoetSplit[1]}_${secondPoetSplit[2]}_${secondPoetSplit[3]}`
-            var searchUrl2 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${secondPoetSplit[0]}%20${secondPoetSplit[1]}%20${secondPoetSplit[2]}%20${firstPoetSplit[3]}&utf8=&format=json`
-        } else {
-            var poet2 = "This poet has not got a name, or it's too long"
-            var url2 = "There is no link"
-            var searchUrl3 = "there is no snippet"
-        }
-
-        let thirdPoet = authors[randomNumberThree];
-        let thirdPoetSplit = thirdPoet.split(" ");
-        console.log(thirdPoet)
-        if (thirdPoetSplit.length === 1) {
-            var poet3 = thirdPoetSplit[0];
-            var url3 = `https://en.wikipedia.org/wiki/${poet3}`;
-            var searchUrl3 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${poet3}&utf8=&format=json`;
-        } else if (thirdPoetSplit.length === 2) {
-            var poet3 = `${thirdPoetSplit[0]} ${thirdPoetSplit[1]}`;
-            var url3 = `https://en.wikipedia.org/wiki/${thirdPoetSplit[0]}_${thirdPoetSplit[1]}`;
-            var searchUrl3 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${thirdPoetSplit[0]}%20${thirdPoetSplit[1]}&utf8=&format=json`
-        } else if (thirdPoetSplit.length === 3) {
-            var poet3 = `${thirdPoetSplit[0]} ${thirdPoetSplit[1]} ${thirdPoetSplit[2]}`;
-            var url3 = `https://en.wikipedia.org/wiki/${thirdPoetSplit[0]}_${thirdPoetSplit[1]}_${thirdPoetSplit[2]}`;
-            var searchUrl3 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${thirdPoetSplit[0]}%20${thirdPoetSplit[1]}%20${thirdPoetSplit[2]}&utf8=&format=json`
-        } else if (thirdPoetSplit.length === 4) {
-            var poet3 = `${thirdPoetSplit[0]} ${thirdPoetSplit[1]} ${thirdPoetSplit[2]} ${thirdPoetSplit[3]}`;
-            var url3 = `https://en.wikipedia.org/wiki/${thirdPoetSplit[0]}_${thirdPoetSplit[1]}_${thirdPoetSplit[2]}_${thirdPoetSplit[3]}`;
-            var searchUrl3 = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${thirdPoetSplit[0]}%20${thirdPoetSplit[1]}%20${thirdPoetSplit[2]}%20${thirdPoetSplit[3]}&utf8=&format=json`
-        } else {
-            var poet3 = "This poet has not got a name, or it's too long";
-            var url2 = "There is no link";
-            var searchUrl3 = "there is no snippet";
-        }
-
-        fetch(searchUrl1)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (parsedResponseWiki) {
-                var snippet1 = `${parsedResponseWiki.query.search[0].snippet}... click on the link to find out more`;
-                displayPoets(poet1, url1, snippet1, "1");
-                //console.log(snippet1)
-            })
-
-        fetch(searchUrl2)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (parsedResponseWiki) {
-                var snippet2 = `${parsedResponseWiki.query.search[0].snippet}... click on the link to find out more`;
-                displayPoets(poet2, url2, snippet2, "2");
-                //console.log(snippet2)
-            })
-
-        fetch(searchUrl3)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (parsedResponseWiki) {
-                var snippet3 = `${parsedResponseWiki.query.search[0].snippet}... click on the link to find out more`;
-                displayPoets(poet3, url3, snippet3, "3");
-                //console.log(snippet3)
-            })
-
-        function displayPoets(poetName, hrefUrl, websiteSnippet, poetNumber) {
-            var element = document.getElementById(`getPoets${poetNumber}`); //this says where you want to put the new text - in this case with 'getPoets'
-            var pOne = document.createElement("h3"); //this says what type of element you want to create
-            var pTwo = document.createElement("a");
-            var pThree = document.createElement("p");
-
-            var useLineOne = document.createTextNode(poetName); //this says that you want to create a 
-            var useLineTwo = document.createTextNode(hrefUrl);
-            var useLineThree = document.createTextNode(websiteSnippet);
-
-            pTwo.title = "Link to Poet's wikipedia";
-            pTwo.href = hrefUrl;
-            pTwo.target = "_blank";
-            pTwo.id = "poemUrl";
-            pOne.appendChild(useLineOne);
-            pTwo.appendChild(useLineTwo);
-            pThree.appendChild(useLineThree);
-            element.appendChild(pOne);
-            element.appendChild(pTwo);
-            element.appendChild(pThree);
-        };
     });
 
-    
-
-
     function checkSubmission() {
-        var authorBox = document.getElementById("author").value
+        var authorBox = $("#author").val()
         console.log(authorBox)
-        var poemBox = document.getElementById("text-area-poem").value
+        var poemBox = $("#text-area-poem").val()
         console.log(poemBox)
 
         if (authorBox.length == 0 || poemBox.length == 0) {
@@ -297,11 +220,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
     };
-
-    var submitButton = document.getElementById ('fancy');
-    submitButton.addEventListener('click', function (event) {
+    $("#fancy").click(function () {
         checkSubmission();
-    });
-
+    })
 }, false);
-
