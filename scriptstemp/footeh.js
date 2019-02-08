da8c6f80af079401bccbfd1c3b4c14fdf13968c3
Andrew Rees <andrewtrevor.rees@gmail.com>
2019-02-08 11:58:09 +0000

    rework with jQuery

--- scriptstemp/footeh.js
+++ scriptstemp/footeh.js
@@ -1,67 +1,29 @@
-// look at theguardian API
-//// access key is: caea3217-7fe3-465b-87c2-25d51493099c
-//// address is: https://content.guardianapis.com/search?api-key=caea3217-7fe3-465b-87c2-25d51493099c
-//get the newest football story and display
-////search for the Tag football eg https://content.guardianapis.com/search?q=football&api-key=caea3217-7fe3-465b-87c2-25d51493099c
-////order by webPublicationDate
-//// need to format webPub (as moment can't be used)
-
-//const request = require("request");
-
-console.log("the file ran!")
-
-
-
-
-
-
-function getLatestStory() {
-    console.log("getlatestStory ran!")
-    let apiKey = "caea3217-7fe3-465b-87c2-25d51493099c"
-    let story = "football"
-    fetch(`https://content.guardianapis.com/search?q=${story}&order-by=newest&api-key=${apiKey}`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (parsedResponse) {
-            console.log(parsedResponse);
-            var webUrl = parsedResponse.response.results[0].webUrl
-            var webTitle = parsedResponse.response.results[0].webTitle
-            var webPub = parsedResponse.response.results[0].webPublicationDate
-            //need something to format the date from utc standard
-            console.log(`parsedResponse: ${webPub}`);
-            displayLatestStory(webUrl, webTitle, webPub)
-        })
-}
-
-
-function displayLatestStory(a, b, c) {
-    console.log("displayLatesStoory ran!")
-    console.log(`This is the url: ${a}`)
-    console.log(`This is the headline: ${b}`)
-    console.log(`And it was published: ${c}`)
-    var pOne = document.createElement("a");
-    var pTwo = document.createElement("p");
-    var pThree = document.createElement("p");
-    var element = document.getElementById("guardianFootball");
-    var useLineOne = document.createTextNode(a);
-    var useLineTwo = document.createTextNode(b);
-    var useLineThree = document.createTextNode(c);
-    pOne.id="url"
-    pOne.href=a
-    pOne.target="_blank"
-    pOne.appendChild(useLineOne);
-    pTwo.appendChild(useLineTwo);
-    pThree.appendChild(useLineThree);
-    element.appendChild(pOne);
-    element.appendChild(pTwo);
-    element.appendChild(pThree);
-
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-    getLatestStory()
-    console.log("The DOM content loaded")
+
+    function getAndDisplayLatestStory() {
+        let apiKey = "caea3217-7fe3-465b-87c2-25d51493099c"
+        let story = "football"
+        fetch(`https://content.guardianapis.com/search?q=${story}&order-by=newest&api-key=${apiKey}`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (parsedResponse) {
+                var webUrl = parsedResponse.response.results[0].webUrl;
+                var webTitle = parsedResponse.response.results[0].webTitle;
+                var webPub = parsedResponse.response.results[0].webPublicationDate;
+                var formattedDate = moment(webPub).format("DD MMM YYYY, HH:MM:SS");
+                formatLatestStory(webUrl, webTitle, formattedDate)
+            });
+    };
+
+    function formatLatestStory(webUrl, webTitle, formattedDate) {
+        var spiel = $("<p>").text("The latest football articles from the Guardian here:").attr("id", "bold")
+        var title = $("<a>").text(webTitle).attr('href', webUrl).attr("target", "_blank")
+        var publishedOn = $("<p>").text(formattedDate);
+        $(".apiReturn").append(spiel, title, publishedOn);
+    };
+    getAndDisplayLatestStory()
+    
 }, false);
 
 
